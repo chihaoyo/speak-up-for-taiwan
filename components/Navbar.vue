fa8e5423f3f71bbb2e4a5310362aec82a5eb0611
chihaoyo <chihaoyo@gmail.com>
2019-01-10 23:33:30 +0800

    Use localePath

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,26 +1,31 @@
 <!-- eslint-disable vue/html-indent vue/max-attributes-per-line -->
-<i18n>
-{
-  "tw": {
-    "home": "返回首頁"
-  },
-  "ja": {
-    "home": "ホーム"
-  },
-  "en": {
-    "home": "Back home"
-  },
-  "fr": {
-    "home": "Revenir"
-  }
-}
-</i18n>
 <template>
-<div class="nav">
-  <nuxt-link v-if="$route.path !== '/'" :to="{ path: '/' }">{{ $t('home') }}</nuxt-link>
-  <nuxt-link key="tw" :to="switchLocalePath('tw')" class="lang">華語</nuxt-link>
-  <nuxt-link key="jp" :to="switchLocalePath('ja')" class="lang">日本語</nuxt-link>
-  <nuxt-link key="en" :to="switchLocalePath('en')" class="lang">English</nuxt-link>
-  <nuxt-link key="fr" :to="switchLocalePath('fr')" class="lang">Français</nuxt-link>
+<div class="navbar">
+  <div class="home">
+    <nuxt-link :to="localePath('index')" class="button">#SpeakUpForTaiwan</nuxt-link>
+  </div>
+  <div class="languages">
+    <nuxt-link key="tw" :to="switchLocalePath('tw')" class="button">華</nuxt-link>
+    <nuxt-link key="jp" :to="switchLocalePath('ja')" class="button">日</nuxt-link>
+    <nuxt-link key="en" :to="switchLocalePath('en')" class="button">Eng</nuxt-link>
+    <nuxt-link key="fr" :to="switchLocalePath('fr')" class="button">Fra</nuxt-link>
+  </div>
 </div>
 </template>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+  > .home {
+    flex-grow: 1;
+  }
+  > .languages {
+    flex-grow: 0;
+    display: flex;
+  }
+}
+.button {
+  display: inline-block;
+  padding: 0.5rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,22 @@
   "tw": {
     "idg-xi": "台灣原住民族致中國習近平主席"
   },
+  "ja": {
+    "idg-xi": "台湾の原住民族から中国国家主席習近平へ"
+  },
   "en": {
     "idg-xi": "Indigenous Peoples of Taiwan to President Xi Jinping of China"
+  },
+  "fr": {
+    "idg-xi": "Les Peuples autochtones de Taïwan au Président Xi Jinping de la Chine"
   }
 }
 </i18n>
 <template>
 <div class="page index">
-  <h1>Speak up for Taiwan.</h1>
+  <h1 style="display: none;">Speak up for Taiwan. #SpeakUpForTaiwan</h1>
   <ul>
-    <li><nuxt-link :to="{ path: '/idg-xi' }">{{ $t('idg-xi') }}</nuxt-link>
+    <li><nuxt-link :to="localePath('idg-xi')">{{ $t('idg-xi') }}</nuxt-link>
     </li>
   </ul>
 </div>
