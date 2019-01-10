4fc903f7c0e4851c598e4e252e301656e38ef9f9
chihaoyo <chihaoyo@gmail.com>
2019-01-10 22:56:02 +0800

    Update Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,26 @@
 <!-- eslint-disable vue/html-indent vue/max-attributes-per-line -->
+<i18n>
+{
+  "tw": {
+    "home": "返回首頁"
+  },
+  "ja": {
+    "home": "ホーム"
+  },
+  "en": {
+    "home": "Back home"
+  },
+  "fr": {
+    "home": "Revenir"
+  }
+}
+</i18n>
 <template>
 <div class="nav">
-  <nuxt-link key="tw" :to="switchLocalePath('tw')" class="lang">中文</nuxt-link>
+  <nuxt-link v-if="$route.path !== '/'" :to="{ path: '/' }">{{ $t('home') }}</nuxt-link>
+  <nuxt-link key="tw" :to="switchLocalePath('tw')" class="lang">華語</nuxt-link>
+  <nuxt-link key="jp" :to="switchLocalePath('ja')" class="lang">日本語</nuxt-link>
   <nuxt-link key="en" :to="switchLocalePath('en')" class="lang">English</nuxt-link>
+  <nuxt-link key="fr" :to="switchLocalePath('fr')" class="lang">Français</nuxt-link>
 </div>
 </template>
